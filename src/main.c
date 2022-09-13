08e1c87471c6179c194d47b3c45d2a625f9089a9
bokfink <bokfink@pm.me>
2022-09-13 17:14:54 +0900

    add sample code for tutorial video part 2

--- src/main.c
+++ src/main.c
@@ -2,33 +2,27 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/zephyr.h>
 
-static const struct device *gpio_ct_dev =
-  DEVICE_DT_GET(DT_NODELABEL(gpio0));
+static const struct gpio_dt_spec led =
+  GPIO_DT_SPEC_GET(DT_NODELABEL(blinking_led), gpios);
 
 void main(void)
 {
-  if (!device_is_ready(gpio_ct_dev)) {
+  if (!device_is_ready(led.port)) {
     return;
   }
 
   int ret;
-  ret = gpio_pin_configure(gpio_ct_dev,
-			   25, GPIO_OUTPUT_ACTIVE);
+  ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
   if (ret != 0) {
     return;
   }
   
   while (true) {
-    ret = gpio_pin_set_raw(gpio_ct_dev, 25, 1);
+    ret = gpio_pin_toggle_dt(&led);
     if (ret != 0) {
       return;
     }
     k_msleep(1000);
 
-    ret = gpio_pin_set_raw(gpio_ct_dev, 25, 0);
-    if (ret != 0) {
-      return;
-    }
-    k_msleep(1000);
   }
 }
